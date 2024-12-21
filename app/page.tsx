5cf53564e05cd6e12f66960321921caac95dea25
kdtTetz <xenosign@naver.com>
2024-12-21 11:44:00 +0900

    241221 nextPage 컴포넌트 확인

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
+  console.log('서버 콘솔 확인 용, 실행 중');
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <Link href="/about">About Us as Link</Link>
+      <br />
+      <a href="/about">About Us as Anchor</a>
     </main>
   );
 }
